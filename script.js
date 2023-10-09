5a0eb16d666002d012f6cf427f159df03c5b24f4
Ravinder <108238004+yadavravi1610@users.noreply.github.com>
2023-10-09 21:13:22 +0530

    Updated the script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var button = document.getElementsByClassName('button');
 var display = document.getElementById('display');
 
 var operand1 = 0;
-var operand2 = 0;
+var operand2 = null;
 var operator = null;
 
 function isOperator(value){
